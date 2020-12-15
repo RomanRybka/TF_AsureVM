ca5bc52b4d743093b5edcb65653d584243ce8bc8
Rybka Roman <rybka.roman@yandex.ru>
2020-12-16 00:33:47 +0300

    Change  main.tf to use CreateVM module

--- main.tf
+++ main.tf
@@ -62,38 +62,11 @@ module "TF_CreateNI" {
 }
 
 # Create a Linux virtual machine
-resource "azurerm_virtual_machine" "vm" {
-  name                  = "${var.prefix}TFVM"
-  location              = var.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [module.TF_CreateNI.NInterface_ID]
-  vm_size               = "Standard_DS1_v2"
-  tags                  = var.tags
-
-  storage_os_disk {
-    name              = "${var.prefix}OsDisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
-  }
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = lookup(var.sku, var.location)
-    version   = "latest"
-  }
-
-  os_profile {
-    computer_name  = "${var.prefix}TFVM"
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
+module "TF_CreateVM" {
+  source        = "./modules/TF_CreateVM"
+  location      = var.location
+  rgname        = azurerm_resource_group.rg.name
+  NInterface_ID = [module.TF_CreateNI.NInterface_ID]
 }
 
 //data "azurerm_public_ip" "ip" {
