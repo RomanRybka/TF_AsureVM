13c2f604e556ea65756afff6df76c14af4685940
Rybka Roman <rybka.roman@yandex.ru>
2020-12-16 00:38:12 +0300

    Fix CreateVM module

--- modules/TF_CreateVM/CreateVM.tf
+++ modules/TF_CreateVM/CreateVM.tf
@@ -2,8 +2,8 @@
 resource "azurerm_virtual_machine" "vm" {
   name                  = "${var.prefix}TFVM"
   location              = var.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [var.NInterface_ID]
+  resource_group_name   = var.rgname
+  network_interface_ids = var.NInterface_ID
   vm_size               = "Standard_DS1_v2"
   tags                  = var.tags
 
